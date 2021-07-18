497ec9da6f1dadaa1954f75ef0f305c0e043f4a5
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-07-18 15:45:44 -0300

    Initial Commit

---
+++ comentarios/forms.py
@@ -0,0 +1,45 @@
+from django.forms import ModelForm
+from .models import Comentario
+import requests
+
+
+class FormComentario(ModelForm):
+    def clean(self):
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        # https://www.google.com/recaptcha/api/siteverify
+        # secret_key
+        # resposta captcha
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': 'YOUR_KEY',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpe Mr. Robot, ocorreu um erro'
+            )
+
+        print(recaptcha_result)
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
+
+        if len(nome) < 5:
+            self.add_error(
+                'nome_comentario',
+                'Nome precisa ter mais que 5 caracteres.'
+            )
+
+    class Meta:
+        model = Comentario
+        fields = ('nome_comentario', 'email_comentario', 'comentario')
